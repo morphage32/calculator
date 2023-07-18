03d1e1bb83e83b760b40b453682e59a8eb65cbd9
morphage32 <mwigand32@yahoo.com>
2023-07-17 18:38:32 -0700

    Simple functions created to execute simple addition, subtraction, multiplication, and division.
    Each function is set up to work with strings as parameters, which is how the operands are intended to be built.

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(left, right) {
+    return +(left) + +(right);
+}
+
+function subtract(left, right) {
+    return left - right;
+}
+
+function multiply(left, right) {
+    return left * right;
+}
+
+function divide(left, right) {
+    return left / right;
+}
