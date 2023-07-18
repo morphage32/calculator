b22c52345f3f06842204477bf4f1b24a74c3776a
morphage32 <mwigand32@yahoo.com>
2023-07-17 18:55:24 -0700

    Operate function added to project.
    Added function will detect which math expression is going to be performed. Variables added for button inputs as well.

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function multiply(left, right) {
 function divide(left, right) {
     return left / right;
 }
+
+function operate(left, right, sign) {
+    if(sign === "+"){
+        return add(left, right);
+    }else if(sign === "-"){
+        return subtract(left, right);
+    }else if(sign === "*"){
+        return multiply(left, right);
+    }else{
+        return divide(left, right);
+    }
+}
+
+let leftNum = '';
+let rightNum = '';
+let sign = '';
